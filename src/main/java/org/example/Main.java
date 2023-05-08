a81405474b3668dd9bd545d3d2a90f22ff3decaa
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-05-08 12:34:32 +0200

    Add project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world!");
+
+        Transcript transcript = new Transcript();
+        transcript.setAudio_url("https://github.com/johnmarty3/JavaAPITutorial/raw/main/Thirsty.mp4");
+
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(transcript);
+
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                .header("Authorization", "9d5864ae89874e689f419b63970c2296")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> postResponse = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+
+        transcript = gson.fromJson(postResponse.body(), Transcript.class);
+
+        System.out.println(transcript.getId());
+
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getId()))
+                .header("Authorization", "9d5864ae89874e689f419b63970c2296")
+                .build();
+
+        while (true) {
+            HttpResponse<String> getResponse = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+            transcript = gson.fromJson(getResponse.body(), Transcript.class);
+
+            System.out.println(transcript.getStatus());
+
+            if ("completed".equals(transcript.getStatus()) || "error".equals(transcript.getStatus())) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+
+        System.out.println("COMPLETED");
+        System.out.println(transcript.getText());
+    }
+}
